708da444c7ef5c2392fd97a37f12a0ba0f68e95e
Michael E Smithzer <michael.smithzer@gmail.com>
2022-05-15 23:57:04 -0400

    Forgot semicolon

--- Program.cs
+++ Program.cs
@@ -10,15 +10,21 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHttpsRedirection();
+    // app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+// app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapRazorPages();
 
